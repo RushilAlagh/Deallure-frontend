f07b465aeb0925de1f56bcfe818b95335fe4f5d5
SairanjanSubudhi <ranjansai1214@gmail.com>
2025-02-28 02:22:40 +0530

    Login page Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-// Remove this import if you don't want to use App.css
-// import './App.css'
-import Signup from './components/Signup/Signup'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
+
 
 function App() {
   return (
-    <div className="app-container">
-      <Signup />
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
